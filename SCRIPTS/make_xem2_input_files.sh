d48802a19f659e1f172df1f09c4e04f4a23fd3cf
Dave Gaskell <gaskelld@jlab.org>
2023-10-20 08:18:22 -0400

    Initial commit

---
+++ SCRIPTS/make_xem2_input_files.sh
@@ -0,0 +1,21 @@
+#!/bin/csh
+# creat input files for a give target
+# 
+# 
+set targ = {$1}
+set min_angle=15.0
+cd ../INP/
+# Loop over angles
+@ i=1
+while ($i <= 131)
+    set angle=`echo "$min_angle+$i*0.2-0.2" | bc` 
+#    echo $angle
+    set angle_name=`echo "$angle" | tr '.' 'p'`
+    set infile = "${angle_name}_${targ}.inp"
+    echo $infile
+    sed -e "s/<angle>/$angle/;s/<targ>/$targ/" < xem2_inp.template >! $infile
+#    cd ..
+#    nice +10 ./run_extern $infile
+#    cd INP
+    @ i++
+end
